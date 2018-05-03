7221797aa109a883f53880974da240fc681b4d7f
Tran Dung <tranngocdung.201194@gmail.com>
2018-05-03 11:26:38 +0700

    them quan tri vien

---
+++ Table/CreateAdmin.sql
@@ -0,0 +1,10 @@
+use QLKS
+Create table QuanTri
+		(
+            tenDangNhap nvarchar(30),
+            matKhau varchar(30),
+            primary key (tenDangNhap, matKhau)
+		)
+
+insert into QuanTri
+values ('sa', 'p@55w0rd_admin');
